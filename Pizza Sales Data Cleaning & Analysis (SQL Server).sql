cd77763e2deb0c42956ac58249d54b94954f162d
sslegoale0 <sslegoale0@gmail.com>
2024-08-12 04:07:13 +0200

    Add files via upload

--- Pizza Sales Data Cleaning & Analysis (SQL Server).sql
+++ Pizza Sales Data Cleaning & Analysis (SQL Server).sql
@@ -13,4 +13,327 @@ FROM orders;
 
 
 
+EXEC sp_columns orders;
+
+
+
 /* 1. Removal of duplicate rows. */
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY pizza_id
+ORDER BY pizza_id) AS row_num
+FROM orders;
+
+WITH duplicate_orders AS (
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY pizza_id
+ORDER BY pizza_id) AS row_num
+FROM orders)
+SELECT *
+FROM duplicate_orders
+WHERE row_num > 1;
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY
+order_id,
+pizza_name_id,
+quantity,
+order_date,
+order_time,
+unit_price,
+total_price,
+pizza_size,
+pizza_category,
+pizza_ingredients,
+pizza_name
+ORDER BY pizza_id) AS row_num
+FROM orders;
+
+WITH duplicate_orders AS (
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY
+order_id,
+pizza_name_id,
+quantity,
+order_date,
+order_time,
+unit_price,
+total_price,
+pizza_size,
+pizza_category,
+pizza_ingredients,
+pizza_name
+ORDER BY pizza_id) AS row_num
+FROM orders)
+SELECT *
+FROM duplicate_orders
+WHERE row_num > 1;
+
+
+
+/* 2. Data formatting & standardisation. */
+
+SELECT DISTINCT pizza_id
+FROM orders
+ORDER BY pizza_id;
+
+EXEC sp_rename 'orders.pizza_id', 'row_id' , 'COLUMN';
+
+SELECT DISTINCT row_id
+FROM orders
+ORDER BY row_id;
+
+
+
+SELECT DISTINCT order_id
+FROM orders
+ORDER BY order_id;
+
+
+
+SELECT DISTINCT pizza_name_id
+FROM orders
+ORDER BY pizza_name_id;
+
+
+
+SELECT DISTINCT quantity
+FROM orders
+ORDER BY quantity;
+
+
+
+SELECT DISTINCT order_date
+FROM orders
+ORDER BY order_date ASC;
+
+SELECT DISTINCT order_date,
+CONVERT(DATE, order_date, 105)
+FROM orders
+ORDER BY order_date ASC;
+
+UPDATE orders
+SET order_date = CONVERT(DATE, order_date, 105)
+ALTER TABLE orders
+ALTER COLUMN order_date DATE;
+
+
+
+SELECT DISTINCT order_time
+FROM orders
+ORDER BY order_time ASC;
+
+SELECT DISTINCT order_time,
+CONVERT(TIME, order_time, 108)
+FROM orders
+ORDER BY order_time;
+
+UPDATE orders
+SET order_time = CONVERT(TIME, order_time, 108)
+ALTER TABLE orders
+ALTER COLUMN order_time TIME;
+
+SELECT order_date,
+order_time,
+CONCAT(order_date, ' ', order_time) AS order_datetime
+FROM orders;
+
+ALTER TABLE orders
+ADD order_datetime DATETIME;
+
+UPDATE orders
+SET order_datetime = CONCAT(CONVERT(NVARCHAR(255), order_date, 111),
+' ',
+CONVERT(NVARCHAR(255), order_time, 108));
+
+SELECT order_date,
+order_time,
+order_datetime
+FROM orders
+ORDER BY order_datetime;
+
+
+
+SELECT DISTINCT unit_price
+FROM orders
+ORDER BY unit_price;
+
+ALTER TABLE orders
+ALTER COLUMN unit_price FLOAT;
+
+
+
+SELECT DISTINCT total_price
+FROM orders
+ORDER BY total_price;
+
+ALTER TABLE orders
+ALTER COLUMN total_price FLOAT;
+
+SELECT DISTINCT total_price,
+unit_price * quantity
+FROM orders
+WHERE total_price <> unit_price * quantity;
+
+
+
+SELECT DISTINCT pizza_size
+FROM orders
+ORDER BY pizza_size;
+
+SELECT DISTINCT RIGHT(pizza_name_id, 1),
+pizza_size
+FROM orders
+WHERE pizza_size NOT IN ('XL', 'XXL');
+
+SELECT DISTINCT RIGHT(pizza_name_id, 2),
+pizza_size
+FROM orders
+WHERE pizza_size = 'XL';
+
+SELECT DISTINCT RIGHT(pizza_name_id, 3),
+pizza_size
+FROM orders
+WHERE pizza_size = 'XXL';
+
+
+
+SELECT DISTINCT pizza_category
+FROM orders
+ORDER BY pizza_category;
+
+
+
+SELECT DISTINCT pizza_ingredients
+FROM orders
+ORDER BY pizza_ingredients;
+
+SELECT DISTINCT pizza_ingredients,
+REPLACE(pizza_ingredients, '?duja', 'Nduja')
+FROM orders
+WHERE pizza_ingredients LIKE '?duja%';
+
+UPDATE orders
+SET pizza_ingredients = REPLACE(pizza_ingredients, '?duja', 'Nduja')
+WHERE pizza_ingredients LIKE '?duja%';
+
+
+
+SELECT DISTINCT pizza_name
+FROM orders
+ORDER BY pizza_name;
+
+SELECT DISTINCT pizza_name,
+pizza_name_id
+FROM orders
+ORDER BY pizza_name;
+
+
+
+/* 3. Imputation of null/blank values. */
+
+-- There are no blank or values in the dataset.
+
+
+
+/* 4. Removal of irrelevant/redundant columns. */
+
+-- There are no irrelevant or redundant columns in the dataset.
+
+
+
+-----------------------------------------------------------------------------------------------------------------
+
+/* Pizza Sales Data Analysis */
+
+/* 1. Total Revenue by Month */
+
+SELECT DATENAME(MONTH, order_date) AS "month",
+ROUND(SUM(total_price), 0) AS total_revenue,
+SUM(quantity) AS total_sales,
+COUNT(DISTINCT order_id) AS total_orders,
+ROUND(SUM(total_price)/COUNT(DISTINCT order_id), 2) AS "average order value",
+ROUND(SUM(total_price)/SUM(quantity), 2) AS "average pizza price",
+ROUND(SUM(quantity)/COUNT(DISTINCT order_id), 2) AS "average pizzas per order"
+FROM orders
+GROUP BY MONTH(order_date), DATENAME(MONTH, order_date)
+ORDER BY MONTH(order_date);
+
+
+
+/* 2. Total Revenue by Weekday */
+
+SELECT DATENAME(WEEKDAY, order_date) AS "weekday",
+ROUND(SUM(total_price), 0) AS total_revenue,
+SUM(quantity) AS total_sales,
+COUNT(DISTINCT order_id) AS total_orders,
+ROUND(SUM(total_price)/COUNT(DISTINCT order_id), 2) AS "average order value",
+ROUND(SUM(total_price)/SUM(quantity), 2) AS "average pizza price",
+ROUND(SUM(quantity)/COUNT(DISTINCT order_id), 2) AS "average pizzas per order"
+FROM orders
+GROUP BY DATEPART(WEEKDAY, order_date), DATENAME(WEEKDAY, order_date)
+ORDER BY DATEPART(WEEKDAY, order_date);
+
+
+
+/* 3. Total Revenue by Hour */
+
+SELECT DATENAME(HOUR, order_time) AS "hour",
+ROUND(SUM(total_price), 0) AS total_revenue,
+SUM(quantity) AS total_sales,
+COUNT(DISTINCT order_id) AS total_orders,
+ROUND(SUM(total_price)/COUNT(DISTINCT order_id), 2) AS "average order value",
+ROUND(SUM(total_price)/SUM(quantity), 2) AS "average pizza price",
+ROUND(SUM(quantity)/COUNT(DISTINCT order_id), 2) AS "average pizzas per order"
+FROM orders
+GROUP BY DATEPART(HOUR, order_time), DATENAME(HOUR, order_time)
+ORDER BY DATEPART(HOUR, order_time);
+
+
+
+/* 4. Total Revenue by Pizza Name */
+
+SELECT pizza_name,
+ROUND(SUM(total_price), 0) AS total_revenue,
+SUM(quantity) AS total_sales,
+COUNT(DISTINCT order_id) AS total_orders,
+ROUND(SUM(total_price)/COUNT(DISTINCT order_id), 2) AS "average order value",
+ROUND(SUM(total_price)/SUM(quantity), 2) AS "average pizza price",
+ROUND(SUM(quantity)/COUNT(DISTINCT order_id), 2) AS "average pizzas per order"
+FROM orders
+GROUP BY pizza_name
+ORDER BY pizza_name;
+
+
+
+/* 5. Total Revenue by Pizza Size */
+
+SELECT pizza_size,
+ROUND(SUM(total_price), 0) AS total_revenue,
+SUM(quantity) AS total_sales,
+COUNT(DISTINCT order_id) AS total_orders,
+ROUND(SUM(total_price)/COUNT(DISTINCT order_id), 2) AS "average order value",
+ROUND(SUM(total_price)/SUM(quantity), 2) AS "average pizza price",
+ROUND(SUM(quantity)/COUNT(DISTINCT order_id), 2) AS "average pizzas per order"
+FROM orders
+GROUP BY pizza_size
+ORDER BY pizza_size;
+
+
+
+/* 6. Total Revenue by Pizza Category */
+
+SELECT pizza_category,
+ROUND(SUM(total_price), 0) AS total_revenue,
+SUM(quantity) AS total_sales,
+COUNT(DISTINCT order_id) AS total_orders,
+ROUND(SUM(total_price)/COUNT(DISTINCT order_id), 2) AS "average order value",
+ROUND(SUM(total_price)/SUM(quantity), 2) AS "average pizza price",
+ROUND(SUM(quantity)/COUNT(DISTINCT order_id), 2) AS "average pizzas per order"
+FROM orders
+GROUP BY pizza_category
+ORDER BY pizza_category;
